4721c8a0649d44f3df6f0282ce3f1be65a73a2a2
ChangNoi <44426331+RioRocker97@users.noreply.github.com>
2020-04-23 18:11:29 +0700

    Start Over 5

---
+++ lib/page/welcome_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class WelcomePage extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return WelcomePageState();
+  }
+}
+
+class WelcomePageState extends State<WelcomePage>{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      body: Center(
+        child: Text("Nothing here yet..."),
+      ),
+    );
+  }
+}
